7b5299efdb24cac3b26b036054ed04a577f54e60
rrchcr8 <rrch.cr8@gmail.com>
2019-03-24 20:29:22 -0400

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyBHP7dUZdN1k2yPL4U3ZQ_Uz4PtGPRAIYo",
+    authDomain: "carlosapp-c11cb.firebaseapp.com",
+    databaseURL: "https://carlosapp-c11cb.firebaseio.com",
+    projectId: "carlosapp-c11cb",
+    storageBucket: "carlosapp-c11cb.appspot.com",
+    messagingSenderId: "243854248754"
+}
+export default config
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  username: string=""
+  password: string=""
+  
+  constructor(public afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  async login(){
+    const {username, password}=this
+    try {
+      //the next line is a "hardcoded" line:
+      const res= await this.afAuth.auth.signInWithEmailAndPassword(username+"@codedamn.com",password)
+      console.log(res)
+    } catch (error) {
+        console.dir(error)
+        if(error.code==="auth/operation-not-allowed"){
+          console.log("user not found. message created by carloncho")
+
+        }
+
+    }
+  }
+}
