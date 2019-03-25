7b0b066896258d79e4f111209200a2f776065074
rrchcr8 <rrch.cr8@gmail.com>
2019-03-24 21:15:43 -0400

    Alert dialogs added & a new tab was created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
 ];
 
 @NgModule({
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import {AlertController} from '@ionic/angular'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -13,23 +15,40 @@ export class RegisterPage implements OnInit {
   cpassword: string=""
   
 
-  constructor(public afAuth: AngularFireAuth) { }
-
-  ngOnInit() {
+  constructor(
+    public afAuth: AngularFireAuth,
+    public alert: AlertController,
+    public router:Router) { }
+    
+  
+    ngOnInit() {
   }
 
  async register(){
     const {username, password, cpassword} = this
     if(password!==cpassword){
+      this.showAlert("Error","Las contraseñas no coinciden")
       return console.error("el password no coincide")
     }
     try {
       const res = await this.afAuth.auth.createUserWithEmailAndPassword(username+"@boa.com",password)
+      this.showAlert("Registro existoso!","Bienvenido")
       console.log(res)
+      this.router.navigate(['/tabs'])
     } catch (error) {
         console.dir(error)
+        this.showAlert("Error",error.message)
     }
 
   }
+ async showAlert(header: string, message: string){
+  const alert = await this.alert.create({
+    header,
+    message,
+    buttons: ["Ok"]
+  })
+  await alert.present()
+}
+
 
 }
